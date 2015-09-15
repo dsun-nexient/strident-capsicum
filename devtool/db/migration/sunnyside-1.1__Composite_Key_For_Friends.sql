1651f7463ba26249cf7a62cd2270fe36fd4c927b
dsun-nexient <dsun@nexient.com>
2015-09-15 17:07:12 -0400

    modified db stuff

---
+++ devtool/db/migration/sunnyside-1.1__Composite_Key_For_Friends.sql
@@ -0,0 +1,3 @@
+
+
+ALTER TABLE `friends`ADD PRIMARY KEY (`profile_id`, `friend_profile_id`);
---
+++ devtool/db/migration/sunnyside-2.0__Admin_Credentials.sql
@@ -0,0 +1,4 @@
+
+
+INSERT INTO `user` (`role`, `username`, `password`, `is_active`) VALUES ('ADMIN', 'admin', 'admin', true);
+INSERT INTO `user` (`role`, `username`, `password`, `is_active`) VALUES ('USER', 'admin', 'admin', true);
