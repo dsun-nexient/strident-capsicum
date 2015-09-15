544247c38c67f08c1c7841fc264af49eb3f250d8
dsun-nexient <dsun@nexient.com>
2015-09-15 17:12:58 -0400

    messy stuff from renaming files

--- devtool/db/migration/sunnyside-1.1__Admin_Credentials.sql
+++
@@ -1,4 +0,0 @@
-
-
-INSERT INTO `user` (`role`, `username`, `password`, `is_active`) VALUES ('ADMIN', 'admin', 'admin', true);
-INSERT INTO `user` (`role`, `username`, `password`, `is_active`) VALUES ('USER', 'admin', 'admin', true);
